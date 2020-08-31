a4ed9c873c0d0437278ce539c0b27ef4bf827417
joeljjoy1908 <joeljjoy.7@gmail.com>
2020-09-01 02:04:57 +0530

    inital validation file

---
+++ validation.py
@@ -0,0 +1,18 @@
+#simple validation
+def valid(v):
+    if len(v) == 4:
+        try:
+            int(val)
+            return "success"
+        except:
+            return "Not a digit"
+    elif len(v) <= 4:
+        return "Less digits entered"
+    elif len(v) >= 4:
+        return "More digits entered"
+    else:
+        return "no digit"
+if __name__ == "__main__":
+    val = input("Enter the pin code : ")
+    print(valid(val))
+    
